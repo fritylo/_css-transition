776af3058d1fd383a370f19d63fe79c229cc3b5a
fritylo <to.fritylo@gmail.com>
2022-09-14 16:26:56 +0300

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php $ver = time(); ?>
+
+<head>
+    <link rel="stylesheet" href="./index.css?ver=<?=$ver?>">
+</head>
+<body>
+    
+    <div class="block">1</div>
+    <div class="block">2</div>
+    <div class="block">3</div>
+
+    <script src="./index.js?ver=<?=$ver?>"></script>
+</body>
